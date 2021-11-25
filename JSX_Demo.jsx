4485db999b38cbdd60833d4a5b2f398d8fc4309b
Superme-Abdiwahid <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-11-25 14:11:11 -0800

    Create sample demo JSX

---
+++ JSX_Demo.jsx
@@ -0,0 +1,22 @@
+import {React} from 'react';
+'use strict';
+
+
+
+/* a export a hello class and return a div with a p tag hello world*/
+
+export default class hello extends React.Component{
+  render(){
+   return(
+     <div>
+     <p>Hello world </p>
+     
+     </div>
+     
+     );
+  }
+  
+  
+}
+
+
